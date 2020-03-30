fix the bug in MAP calculation

mmdet/apis/inference.py
@@ -10,6 +10,8 @@ import pdb
 from mmdet.core import get_classes
 from mmdet.datasets.pipelines import Compose
 from mmdet.models import build_detector
+from mmcv.image import imread, imwrite
+import cv2
 
 
 def init_detector(config, checkpoint=None, device='cuda:0'):
@@ -190,7 +192,72 @@ def calc_map(map_iou, map_label):
     return map_area / len_positive
 
 
-def show_result_plus_acc(img, result, class_names, gt_bboxes, gt_labels,
+def imshow_det_bboxes(img,
+                      bboxes,
+                      labels,
+                      class_names=None,
+                      score_thr=0,
+                      thickness=2,
+                      font_scale=1,
+                      show=True,
+                      win_name='',
+                      wait_time=0,
+                      out_file=None):
+    """Draw bboxes and class labels (with scores) on an image.
+    Args:
+        img (str or ndarray): The image to be displayed.
+        bboxes (ndarray): Bounding boxes (with scores), shaped (n, 4) or
+            (n, 5).
+        labels (ndarray): Labels of bboxes.
+        class_names (list[str]): Names of each classes.
+        score_thr (float): Minimum score of bboxes to be shown.
+        bbox_color (str or tuple or :obj:`Color`): Color of bbox lines.
+        text_color (str or tuple or :obj:`Color`): Color of texts.
+        thickness (int): Thickness of lines.
+        font_scale (float): Font scales of texts.
+        show (bool): Whether to show the image.
+        win_name (str): The window name.
+        wait_time (int): Value of waitKey param.
+        out_file (str or None): The filename to write the image.
+    """
+    assert bboxes.ndim == 2
+    assert labels.ndim == 1
+    assert bboxes.shape[0] == labels.shape[0]
+    assert bboxes.shape[1] == 4 or bboxes.shape[1] == 5
+    img = imread(img)
+
+    if score_thr > 0:
+        assert bboxes.shape[1] == 5
+        scores = bboxes[:, -1]
+        inds = scores > score_thr
+        bboxes = bboxes[inds, :]
+        labels = labels[inds]
+
+    cm = plt.cm.get_cmap('RdYlBu')
+    text_color = (0, 255, 0)
+
+    for bbox, label in zip(bboxes, labels):
+        bbox_int = bbox.astype(np.int32)
+        left_top = (bbox_int[0], bbox_int[1])
+        right_bottom = (bbox_int[2], bbox_int[3])
+        bbox_color = cm(label / 90.0)
+        bbox_color = (int(bbox_color[0]*255), int(bbox_color[1]*255), int(bbox_color[2]*255))
+        cv2.rectangle(
+            img, left_top, right_bottom, bbox_color, thickness=thickness)
+        label_text = class_names[
+            label] if class_names is not None else 'cls {}'.format(label)
+        if len(bbox) > 4:
+            label_text += '|{:.02f}'.format(bbox[-1])
+        cv2.putText(img, label_text, (bbox_int[0], bbox_int[1] - 2),
+                    cv2.FONT_HERSHEY_COMPLEX, font_scale, text_color)
+
+    if show:
+        imshow(img, win_name, wait_time)
+    if out_file is not None:
+        imwrite(img, out_file)
+
+
+def show_result_plus_acc(img, result, class_names, gt_bboxes, gt_labels, map_data,
                          score_thr=0.3, wait_time=0, show=True, out_file=None):
     """Visualize the detection results on the image.
 
@@ -199,8 +266,9 @@ def show_result_plus_acc(img, result, class_names, gt_bboxes, gt_labels,
         result (tuple[list] or list): The detection result, can be either
             (bbox, segm) or just bbox.
         class_names (list[str] or tuple[str]): A list of class names.
-        gt_labels: ground truth (labels).
         gt_bboxes: ground truth (bboxes).
+        gt_labels: ground truth (labels).
+        map_data: data for map.
         score_thr (float): The threshold to visualize the bboxes and masks.
         wait_time (int): Value of waitKey param.
         show (bool, optional): Whether to show the image with opencv or not.
@@ -238,7 +306,7 @@ def show_result_plus_acc(img, result, class_names, gt_bboxes, gt_labels,
         if out_file:
             gt_labels = np.array([0] * len(gt_bboxes))
             labels = np.array([0] * len(bboxes))
-            mmcv.imshow_det_bboxes(
+            imshow_det_bboxes(
                 img.copy(),
                 gt_bboxes,
                 gt_labels,
@@ -247,7 +315,7 @@ def show_result_plus_acc(img, result, class_names, gt_bboxes, gt_labels,
                 show=show,
                 wait_time=wait_time,
                 out_file=out_file + '_gt.jpg')
-            mmcv.imshow_det_bboxes(
+            imshow_det_bboxes(
                 img,
                 bboxes,
                 labels,
@@ -265,11 +333,11 @@ def show_result_plus_acc(img, result, class_names, gt_bboxes, gt_labels,
                 map_iou.append(max_iou)
             else:
                 map_iou.append(0)
-        return 0, float(sum(map_iou)) / len(map_iou), 0
+        return 0, float(sum(map_iou)) / len(map_iou), 0, None
     else:
         gt_labels = gt_labels.numpy() - 1
     if out_file:
-        mmcv.imshow_det_bboxes(
+        imshow_det_bboxes(
             img.copy(),
             gt_bboxes,
             gt_labels,
@@ -278,7 +346,7 @@ def show_result_plus_acc(img, result, class_names, gt_bboxes, gt_labels,
             show=show,
             wait_time=wait_time,
             out_file=out_file + '_gt.jpg')
-        mmcv.imshow_det_bboxes(
+        imshow_det_bboxes(
             img,
             bboxes,
             labels,
@@ -294,7 +362,19 @@ def show_result_plus_acc(img, result, class_names, gt_bboxes, gt_labels,
     class_acc = 0
     map_iou = []
     map_label = []
+    flag0 = np.zeros(len(class_names))
+    flag1 = np.zeros(len(class_names))
     for i in range(0, len(gt_labels)):
+        if map_data[1][gt_labels[i]] is None:
+            if flag1[gt_labels[i]] == 0:
+                map_data[1][gt_labels[i]] = [np.array([gt_bboxes[i]])]
+                flag1[gt_labels[i]] = 1
+        else:
+            if flag1[gt_labels[i]] == 0:
+                map_data[1][gt_labels[i]].append(np.array([gt_bboxes[i]]))
+                flag1[gt_labels[i]] = 1
+            else:
+                map_data[1][gt_labels[i]][-1] = np.concatenate((map_data[1][gt_labels[i]][-1], np.array([gt_bboxes[i]])))
         match_index, max_iou = iou_vector(bboxes, gt_bboxes[i])
         if match_index > -1:
             map_iou.append(max_iou)
@@ -304,14 +384,36 @@ def show_result_plus_acc(img, result, class_names, gt_bboxes, gt_labels,
                 map_label.append(1)
             else:
                 map_label.append(0)
+    for i in range(0, len(labels)):
+        if map_data[0][labels[i]] is None:
+            if flag0[labels[i]] == 0:
+                map_data[0][labels[i]] = [np.array([bboxes[i]])]
+                flag0[labels[i]] = 1
+        else:
+            if flag0[labels[i]] == 0:
+                map_data[0][labels[i]].append(np.array([bboxes[i]]))
+                flag0[labels[i]] = 1
+            else:
+                map_data[0][labels[i]][-1] = np.concatenate((map_data[0][labels[i]][-1], np.array([bboxes[i]])))
+    for i in range(len(class_names)):
+        if flag0[i] == 0 and flag1[i] == 1:
+            if len(map_data[1][i]) == 1:
+                map_data[1][i] = None
+            else:
+                map_data[1][i].pop()
+        if flag0[i] == 1 and flag1[i] == 0:
+            if map_data[1][i] is None:
+                map_data[1][i] = [np.array([])]
+            else:
+                map_data[1][i].append(np.array([]))
     if class_acc == 0:
         iou_acc = 0
-        map_area = 0
+        iou_acc2 = 0
     else:
+        iou_acc2 = iou_acc / len(gt_labels)
         iou_acc /= class_acc
         class_acc /= len(gt_labels)
-        map_area = calc_map(np.array(map_iou), np.array(map_label))
-    return class_acc, iou_acc, map_area
+    return class_acc, iou_acc, iou_acc2, map_data
 
 
 def show_result_pyplot(img,

mmdet/apis/train.py
@@ -24,6 +24,7 @@ import threading
 from datetime import datetime
 import scipy.stats as st
 import shutil
+from mmdet.core import eval_map_attack
 
 
 class ThreadingWithResult(threading.Thread):
@@ -32,7 +33,7 @@ class ThreadingWithResult(threading.Thread):
         super(ThreadingWithResult, self).__init__()
         self.func = func
         self.args = args
-        self.result = -1 * np.ones(4)
+        self.result = [-1 * np.ones(6), None]
 
     def run(self):
         self.result = self.func(*self.args)
@@ -290,6 +291,7 @@ def attack_detector(args, model, cfg, dataset):
     print(str(datetime.now()) + ' - INFO - Imgs per GPU: ', cfg.data.imgs_per_gpu)
     print(str(datetime.now()) + ' - INFO - Workers per GPU: ', cfg.data.workers_per_gpu)
     print(str(datetime.now()) + ' - INFO - Momentum: ', args.momentum)
+    print(str(datetime.now()) + ' - INFO - Epsilon: ', args.epsilon)
     infer_model = load_model(args)
     attack_loader = build_dataloader(dataset, cfg.data.imgs_per_gpu, cfg.data.workers_per_gpu, cfg.gpus, dist=False)
     model = MMDataParallel(model, device_ids=range(cfg.gpus)).cuda()
@@ -300,6 +302,8 @@ def attack_detector(args, model, cfg, dataset):
                 shutil.rmtree(os.path.join(args.save_path, f))
             else:
                 os.remove(os.path.join(args.save_path, f))
+    class_names = infer_model.CLASSES
+    num_of_classes = len(infer_model.CLASSES)
     max_batch = min(attack_loader.__len__(), args.max_attack_batches)
     pbar_outer = tqdm(total=max_batch)
     pbar_inner = tqdm(total=args.num_attack_iter)
@@ -311,6 +315,7 @@ def attack_detector(args, model, cfg, dataset):
     dot_product = 0
     conv_kernel = None
     with_mask = hasattr(model.module, 'mask_head') and model.module.mask_head is not None
+    MAP_data = [[[None] * num_of_classes, [None] * num_of_classes], [[None] * num_of_classes, [None] * num_of_classes]]
     if args.kernel_size != 0:
         conv_kernel = conv_layer(args)
     for i, data in enumerate(attack_loader):
@@ -322,7 +327,13 @@ def attack_detector(args, model, cfg, dataset):
         for j in range(0, len(imgs.data)):
             imgs.data[j] = imgs.data[j].cuda()
             if args.visualize:
-                visualize_modification(args, infer_model, imgs.data[j], j, data['img_meta'].data[j])
+                if args.model_name == 'rpn_r50_fpn_1x':
+                    visualize_modification(args, infer_model, copy.deepcopy(imgs.data[j]), j,
+                                           data['img_meta'].data[j], data['gt_bboxes'].data[j])
+                else:
+                    visualize_modification(args, infer_model, copy.deepcopy(imgs.data[j]), j,
+                                           data['img_meta'].data[j], data['gt_bboxes'].data[j],
+                                           data['gt_labels'].data[j])
             imgs.data[j] = imgs.data[j].detach()
             imgs.data[j].requires_grad = True
             number_of_images += imgs.data[j].size()[0]
@@ -377,7 +388,13 @@ def attack_detector(args, model, cfg, dataset):
                             num_attack_iter * torch.sign(update_direction)
                 imgs.data[j] = imgs.data[j].detach()
                 if args.visualize:
-                    visualize_modification(args, infer_model, copy.deepcopy(imgs.data[j]), j, data['img_meta'].data[j])
+                    if args.model_name == 'rpn_r50_fpn_1x':
+                        visualize_modification(args, infer_model, copy.deepcopy(imgs.data[j]), j,
+                                               data['img_meta'].data[j], data['gt_bboxes'].data[j])
+                    else:
+                        visualize_modification(args, infer_model, copy.deepcopy(imgs.data[j]), j,
+                                               data['img_meta'].data[j], data['gt_bboxes'].data[j],
+                                               data['gt_labels'].data[j])
                 imgs.data[j].requires_grad = True
                 if args.visualize and _ > 0:
                     dot_product += torch.sum(update_direction.view(-1) /
@@ -392,18 +409,21 @@ def attack_detector(args, model, cfg, dataset):
                 t = ThreadingWithResult(visualize_all_images_plus_acc, args=(args, infer_model,
                                                                              imgs.data[j], raw_imgs.data[j],
                                                                              data['img_meta'].data[j],
-                                                                             data['gt_bboxes'].data[j]))
+                                                                             data['gt_bboxes'].data[j],
+                                                                             MAP_data))
             else:
                 t = ThreadingWithResult(visualize_all_images_plus_acc, args=(args, infer_model,
                                                                              imgs.data[j], raw_imgs.data[j],
                                                                              data['img_meta'].data[j],
                                                                              data['gt_bboxes'].data[j],
+                                                                             MAP_data,
                                                                              data['gt_labels'].data[j]))
             t.start()
             t.join()
             statistics_result = t.get_result()
-            if statistics_result[0] >= 0:
-                statistics += statistics_result
+            if statistics_result[0][0] >= 0:
+                statistics += statistics_result[0]
+                MAP_data = statistics_result[1]
             else:
                 print("Error! Results were not fetched!")
         pbar_outer.update(1)
@@ -415,17 +435,31 @@ def attack_detector(args, model, cfg, dataset):
     acc_before_attack /= max_batch
     acc_under_attack /= max_batch
     statistics /= number_of_images
+
     if args.neglect_raw_stat and args.experiment_index > args.resume_experiment:
         pass
     else:
         args.class_accuracy_before_attack = 100 * statistics[0]
         args.IoU_accuracy_before_attack = 100 * statistics[1]
-        args.MAP_before_attack = statistics[2]
+        args.IoU_accuracy_before_attack2 = 100 * statistics[2]
+        if MAP_data[0] is None:
+            args.MAP_before_attack = 0
+        else:
+            args.MAP_before_attack = eval_map_attack(MAP_data[0][0], MAP_data[0][1], len(class_names),
+                                                     scale_ranges=None, iou_thr=0.5,
+                                                     dataset=class_names, print_summary=True)[0]
     args.class_accuracy_under_attack = 100 * statistics[3]
     args.IoU_accuracy_under_attack = 100 * statistics[4]
-    args.MAP_under_attack = statistics[5]
+    args.IoU_accuracy_under_attack2 = 100 * statistics[5]
+    if MAP_data[1] is None:
+        args.MAP_under_attack = 0
+    else:
+        args.MAP_under_attack = eval_map_attack(MAP_data[1][0], MAP_data[1][1], len(class_names),
+                                                scale_ranges=None, iou_thr=0.5,
+                                                dataset=class_names, print_summary=True)[0]
     args.class_accuracy_decrease = args.class_accuracy_before_attack - args.class_accuracy_under_attack
     args.IoU_accuracy_decrease = args.IoU_accuracy_before_attack - args.IoU_accuracy_under_attack
+    args.IoU_accuracy_decrease2 = args.IoU_accuracy_before_attack2 - args.IoU_accuracy_under_attack2
     args.MAP_decrease = 100 * (args.MAP_before_attack - args.MAP_under_attack)
     print("Class & IoU accuracy before attack = %g %g" % (args.class_accuracy_before_attack,
                                                           args.IoU_accuracy_before_attack))

tools/attack.py
@@ -51,7 +51,7 @@ def visualize():
     print('[INFO]Done.')
 
 
-def visualize_modification(args, model, imgs, index, metadata):
+def visualize_modification(args, model, imgs, index, metadata, gt_bboxes, gt_labels=None):
     imgs = imgs.permute(0, 2, 3, 1)[:, :, :, [2, 1, 0]]
     num_of_imgs = imgs.size()[0]
     imgs = imgs.detach().cpu().numpy()
@@ -64,8 +64,13 @@ def visualize_modification(args, model, imgs, index, metadata):
             os.makedirs(args.save_path + str(index) + '/' + str(img_index))
         save_path = args.save_path + str(index) + '/' + str(img_index) + '/' + str(datetime.datetime.now()) + '.jpg'
         img = mmcv.imread(imgs[img_index])
+        result = inference_detector(model, img)
+        show_result_plus_acc(img, result, model.CLASSES, gt_bboxes[img_index], gt_labels[img_index],
+                             [[[None] * num_of_classes, [None] * num_of_classes],
+                              [[None] * num_of_classes, [None] * num_of_classes]], show=False,
+                             out_file=save_path)
         # show_result(imgs[img_index], result, model.CLASSES, show=False, out_file=save_path)
-        mmcv.imwrite(img, save_path)
+        # mmcv.imwrite(img, save_path)
 
 
 def visualize_img(model, img, save_path):
@@ -95,22 +100,24 @@ def visualize_all_images(args, model, imgs, raw_imgs, metadata):
     return
 
 
-def visualize_img_plus_acc(model, img, metadata, gt_bboxes, gt_labels, save_path):
+def visualize_img_plus_acc(model, img, metadata, gt_bboxes, gt_labels, save_path, map_data):
     img_mean = metadata['img_norm_cfg']['mean'][::-1]
     img_std = metadata['img_norm_cfg']['std'][::-1]
     img = img * img_std + img_mean
     result = inference_detector(model, img)
-    return show_result_plus_acc(img, result, model.CLASSES, gt_bboxes, gt_labels, show=False, out_file=save_path)
+    return show_result_plus_acc(img, result, model.CLASSES, gt_bboxes, gt_labels,
+                                map_data, show=False, out_file=save_path)
 
 
-def visualize_all_images_plus_acc(args, model, imgs, raw_imgs, metadata, gt_bboxes, gt_labels=None):
+def visualize_all_images_plus_acc(args, model, imgs, raw_imgs, metadata, gt_bboxes, map_data, gt_labels=None):
     imgs = imgs.permute(0, 2, 3, 1)[:, :, :, [2, 1, 0]]
     raw_imgs = raw_imgs.permute(0, 2, 3, 1)[:, :, :, [2, 1, 0]]
     imgs = imgs.detach().cpu().numpy()
     raw_imgs = raw_imgs.numpy()
     raw_class_acc, raw_iou_acc = 0, 0
     class_acc, iou_acc = 0, 0
-    raw_map_area, map_area = 0, 0
+    raw_iou_acc2 = 0
+    iou_acc2 = 0
     if gt_labels is None:
         gt_labels = [-1] * np.shape(imgs)[0]
     for index in range(0, np.shape(imgs)[0]):
@@ -118,29 +125,29 @@ def visualize_all_images_plus_acc(args, model, imgs, raw_imgs, metadata, gt_bbox
         (_, filename) = os.path.split(raw_filename)
         filename = filename.split('.', 2)[0]
         if torch.rand(1) < args.save_ratio:
-            raw_class_acc_image, raw_iou_acc_image, raw_map_area_image = visualize_img_plus_acc(
+            raw_class_acc_image, raw_iou_acc_image, raw_iou_acc_image2, map_data[0] = visualize_img_plus_acc(
                 model, raw_imgs[index], metadata[index], gt_bboxes[index], gt_labels[index],
-                args.save_path + filename)
-            class_acc_image, iou_acc_image, map_area_image = visualize_img_plus_acc(
+                args.save_path + filename, map_data[0])
+            class_acc_image, iou_acc_image, iou_acc_image2, map_data[1] = visualize_img_plus_acc(
                 model, imgs[index], metadata[index], gt_bboxes[index], gt_labels[index],
-                args.save_path + filename + '_attack')
+                args.save_path + filename + '_attack', map_data[1])
         else:
             if args.neglect_raw_stat and args.experiment_index > args.resume_experiment:
                 raw_class_acc_image = 0
                 raw_iou_acc_image = 0
-                raw_map_area_image = 0
+                raw_iou_acc_image2 = 0
             else:
-                raw_class_acc_image, raw_iou_acc_image, raw_map_area_image = visualize_img_plus_acc(
-                    model, raw_imgs[index], metadata[index], gt_bboxes[index], gt_labels[index], None)
-            class_acc_image, iou_acc_image, map_area_image = visualize_img_plus_acc(
-                model, imgs[index], metadata[index], gt_bboxes[index], gt_labels[index], None)
+                raw_class_acc_image, raw_iou_acc_image, raw_iou_acc_image2, map_data[0] = visualize_img_plus_acc(
+                    model, raw_imgs[index], metadata[index], gt_bboxes[index], gt_labels[index], None, map_data[0])
+            class_acc_image, iou_acc_image, iou_acc_image2, map_data[1] = visualize_img_plus_acc(
+                model, imgs[index], metadata[index], gt_bboxes[index], gt_labels[index], None, map_data[1])
         raw_class_acc += raw_class_acc_image
         raw_iou_acc += raw_iou_acc_image
-        raw_map_area += raw_map_area_image
+        raw_iou_acc2 += raw_iou_acc_image2
         class_acc += class_acc_image
         iou_acc += iou_acc_image
-        map_area += map_area_image
-    return np.array([raw_class_acc, raw_iou_acc, raw_map_area, class_acc, iou_acc, map_area])
+        iou_acc2 += iou_acc_image2
+    return np.array([raw_class_acc, raw_iou_acc, raw_iou_acc2, class_acc, iou_acc, iou_acc2]), map_data
 
 
 def attack(args, datasets):
@@ -204,9 +211,10 @@ if __name__ == "__main__":
     result_dict_list = []
     args_raw = parse_args()
     save_keys = ['epsilon', 'loss_keys', 'num_attack_iter', 'momentum', 'kernel', 'kernel_size', 'MAP_decrease',
-                 'class_accuracy_decrease', 'IoU_accuracy_decrease', 'MAP_before_attack', 'MAP_under_attack',
-                 'class_accuracy_before_attack', 'class_accuracy_under_attack', 'IoU_accuracy_before_attack',
-                 'IoU_accuracy_under_attack', 'model_name', 'config', 'work_dir', 'gpus', 'imgs_per_gpu',
+                 'class_accuracy_decrease', 'IoU_accuracy_decrease', 'IoU_accuracy_decrease2', 'MAP_before_attack',
+                 'MAP_under_attack', 'class_accuracy_before_attack', 'class_accuracy_under_attack',
+                 'IoU_accuracy_before_attack', 'IoU_accuracy_under_attack', 'IoU_accuracy_before_attack2',
+                 'IoU_accuracy_under_attack2', 'model_name', 'config', 'work_dir', 'gpus', 'imgs_per_gpu',
                  'max_attack_batches', 'seed', 'model_path', 'save_path']
     search_dict = ['epsilon', 'loss_keys', 'num_attack_iter', 'momentum', 'kernel', 'kernel_size']
     search_values = [[16.0],
@@ -217,10 +225,10 @@ if __name__ == "__main__":
                      [0, 5, 11, 15]]
     # search_values = [[16.0],
     #                  [['loss_rpn_bbox', 'loss_cls']],
-    #                  [5],
-    #                  [0],
+    #                  [10],
+    #                  [2],
     #                  ['Gaussian'],
-    #                  [11]]
+    #                  [15]]
     if args_raw.model_name == 'rpn_r50_fpn_1x':
         search_values = [[16.0],
                          [['loss_rpn_bbox', 'loss_rpn_cls']],