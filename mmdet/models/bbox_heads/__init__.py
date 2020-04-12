932c288e0d92aab83a6c3d1f4baa9504391a48b8
Yao <feng-y16@mails.tsinghua.edu.cn>
2020-04-12 17:25:48 +0800

    add yolov3

--- mmdet/models/bbox_heads/__init__.py
+++ mmdet/models/bbox_heads/__init__.py
@@ -3,5 +3,5 @@ from .convfc_bbox_head import ConvFCBBoxHead, SharedFCBBoxHead
 from .double_bbox_head import DoubleConvFCBBoxHead
 
 __all__ = [
-    'BBoxHead', 'ConvFCBBoxHead', 'SharedFCBBoxHead', 'DoubleConvFCBBoxHead'
+    'BBoxHead', 'ConvFCBBoxHead', 'SharedFCBBoxHead', 'DoubleConvFCBBoxHead', 'YoloHead'
 ]
